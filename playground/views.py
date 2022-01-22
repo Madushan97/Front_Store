52dbe0fb1d1898971819885b8dcab6589344e6ba
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-01-23 03:31:43 +0530

    created view function, mapping review and url

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# view function basically do is take a requset from client side and respond to it (Request Handler)
+
+# some other framworks call it as actions
+
+def say_Hello( request ):
+
+    # pull data from DB
+    # transform
+    # send Email
+    return HttpResponse('Hello World :)')
