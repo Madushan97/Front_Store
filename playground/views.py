6c5080223944ec7b806a8f8500b3faad791ffd57
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-03-01 10:14:59 +0530

    selecting fields to query

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
@@ -41,7 +41,30 @@ def say_hello(request):
     # products : inventory < 10 AND price < 20
     # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
     # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
-    queryset = Product.objects.filter(Q(inventory__lt=10) | ~Q(unit_price__lt=20))
+    # queryset = Product.objects.filter(Q(inventory__lt=10) | ~Q(unit_price__lt=20))
 
-        
-    return render(request, 'hello.html', {'name': 'Madushan', 'products': list(queryset)})
+    # where inventory = price
+    # queryset = Product.objects.filter(inventory = F('unit_price'))
+
+    # sorting
+    # queryset = Product.objects.order_by('-title')
+    # return render(request, 'hello.html', {'name': 'Madushan', 'products': list(queryset)})
+    # queryset = Product.objects.order_by('-title')
+    # sorting multiple products
+    # queryset = Product.objects.order_by('unit_price', '-title')
+    # queryset = Product.objects.order_by('unit_price')[0]
+    # queryset = Product.objects.earliest('unit_price')
+    # descending
+    # queryset = Product.objects.latest('unit_price')
+
+    # limiting results
+    # queryset = Product.objects.all()[:5]
+
+    #filtering values
+    # getting dictionaries as output
+    # queryset = Product.objects.values('id', 'title', 'collection__title')
+
+    # getting tuples as output
+    queryset = Product.objects.values_list('id', 'title', 'collection__title')
+    return render(request, 'hello.html', {'name': 'Madushan', 'products': list(queryset)})     
+   
